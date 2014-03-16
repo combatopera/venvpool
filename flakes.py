929cbb0fd24a6f55ab2ecebe849faea834cd0954
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-03-16 16:55:42 +0000

    port to find command

--- flakes.py
+++ flakes.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
+#!/bin/bash
 
-import os, subprocess, sys, re
-
-def main():
-    top = '.'
-    flakesregex = []
-    f = open(os.path.join(top, '.flakesignore'))
-    try:
-      for line in f:
-        line, = line.splitlines()
-        flakesregex.append(line)
-    finally:
-      f.close()
-    if flakesregex:
-      flakesregex = re.compile('|'.join(flakesregex))
-      def flakesaccept(path):
-        return flakesregex.search(path[len(top) + len(os.sep):].replace(os.sep, '/')) is None
-    else:
-      flakesaccept = lambda path: True
-    paths = []
-    for dirpath, dirnames, filenames in os.walk(top):
-      paths.extend(sorted(os.path.join(dirpath, n) for n in filenames if n.endswith('.py')))
-      dirnames.sort()
-    sys.exit(subprocess.call(['pyflakes'] + [p for p in paths if flakesaccept(p)]))
-
-if __name__ == '__main__':
-  main()
+find -name '*.py' -not '(' $(cat .flakesignore) ')' -exec pyflakes '{}' +
