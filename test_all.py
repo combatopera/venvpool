f1678608d2409f887dba0e74fc522f4c8472b9ff
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-03-16 15:19:09 +0000

    repurpose python script as pyflakes runner

--- test_all.py
+++ test_all.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-import unittest, os, subprocess, sys, re
+import os, subprocess, sys, re
 
-class TestAll(unittest.TestCase):
-
-  def test_all(self):
-    thispath = __file__ # Observe we intentionally do not follow symlink(s).
-    top, thisname = os.path.split(thispath)
+def main():
+    top = '.'
     flakesregex = []
     f = open(os.path.join(top, '.flakesignore'))
     try:
@@ -25,21 +22,7 @@ class TestAll(unittest.TestCase):
     for dirpath, dirnames, filenames in os.walk(top):
       paths.extend(sorted(os.path.join(dirpath, n) for n in filenames if n.endswith('.py')))
       dirnames.sort()
-    subprocess.call(['pyflakes'] + [p for p in paths if flakesaccept(p)])
-    codes = []
-    for path in paths:
-      name = os.path.basename(path)
-      if thisname == name:
-        pass
-      elif name.startswith('test_'):
-        print >> sys.stderr, path
-        codes.append(subprocess.call(path))
-      elif name.startswith('test'):
-        print >> sys.stderr, path
-        print >> sys.stderr, 'F'
-        codes.append(1)
-    print >> sys.stderr, thispath
-    self.assertTrue(not codes or set([0]) == set(codes))
+    sys.exit(subprocess.call(['pyflakes'] + [p for p in paths if flakesaccept(p)]))
 
 if __name__ == '__main__':
-  unittest.main()
+  main()
