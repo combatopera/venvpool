b73f03cb0bec587d0000d6dc2e2e3b020bc1c569
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-03-28 12:29:04 +0000

    check nothing relies on old-style division

---
+++ divcheck.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys, re, ast
+
+def main():
+    for path in sys.argv[1:]:
+        f = open(path)
+        try:
+            text = f.read()
+        finally:
+            f.close()
+        for node in ast.walk(ast.parse(text)):
+            if 'Div' == type(node).__name__:
+                hasdiv = True
+                break
+        else:
+            hasdiv = False
+        if hasdiv and re.search('^from __future__ import division$', text, flags = re.MULTILINE) is None:
+            raise Exception(path)
+
+if '__main__' == __name__:
+    main()
