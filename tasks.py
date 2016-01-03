5f3ada5d53e937fd3ab72759450b8f1c615ede4d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-01-03 16:03:11 +0000

    port to python

--- tasks.py
+++ tasks.py
@@ -1,11 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env python
 
-while ! [[ -e .hg || -e .svn ]]; do cd ..; done
+# Copyright 2014 Andrzej Cichocki
 
-for tag in XXX TODO FIXME; do
+# This file is part of runpy.
+#
+# runpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# runpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with runpy.  If not, see <http://www.gnu.org/licenses/>.
 
-  find '(' -name '*.py' -or -name '*.pyx' ')' -exec pcregrep --color=always "$tag LATER" '{}' +
+import os, subprocess
+from tests import findfiles
 
-  find '(' -name '*.py' -or -name '*.pyx' ')' -exec pcregrep --color=always "$tag(?! LATER)" '{}' +
+def main():
+    while not (os.path.exists('.hg') or os.path.exists('.svn')):
+        os.chdir('..')
+    paths = list(findfiles('.py', '.pyx'))
+    for tag in 'XXX', 'TODO', 'FIXME':
+        subprocess.call(['pcregrep', '--color=always', tag + ' LATER'] + paths)
+        subprocess.call(['pcregrep', '--color=always', tag + '(?! LATER)'] + paths)
 
-done
+if '__main__' == __name__:
+    main()
