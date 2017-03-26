ba98b2386b0b3077ff80ccd0f067040e06227fc5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-26 18:22:31 +0100

    undup

--- pkg/inpkg.py
+++ pkg/inpkg.py
@@ -4,9 +4,8 @@ import sys
 from util import stderr
 
 def main():
-    stderr("sys.path: %s" % sys.path)
-    stderr("sys.executable: %s" % sys.executable)
-    stderr("sys.argv: %s" % sys.argv)
+    for code in 'sys.path', 'sys.executable', 'sys.argv':
+        stderr("%s: %s" % (code, eval(code)))
 
 if '__main__' == __name__:
     main()
