1f40ee21f7c7b20eb387a0439dc1bb7dd5e25181
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-10-04 22:02:19 +0100

    get last published version

---
+++ release.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# Copyright 2013, 2014, 2015, 2016, 2017 Andrzej Cichocki
+
+# This file is part of pyven.
+#
+# pyven is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyven is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyven.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyvenimpl.projectinfo import ProjectInfo
+from urllib.error import HTTPError
+import urllib.request, xml.dom.minidom as dom, re, os, sys
+
+def textcontent(node):
+    def iterparts(node):
+        value = node.nodeValue
+        if value is None:
+            for child in node.childNodes:
+                for part in iterparts(child):
+                    yield part
+        else:
+            yield value
+    return ''.join(iterparts(node))
+
+def main():
+    args = sys.argv[1:]
+    if args:
+        path, = args
+        path = os.path.abspath(path)
+    else:
+        path = os.getcwd()
+    info = ProjectInfo(path)
+    try:
+        with urllib.request.urlopen("https://pypi.org/simple/%s/" % info['name']) as f:
+            doc = dom.parseString(f.read())
+        last = max(int(re.search('[0-9]+', textcontent(a)).group()) for a in doc.getElementsByTagName('a'))
+    except HTTPError as e:
+        if 404 != e.code:
+            raise
+        last = 0
+    print(last)
+
+if '__main__' == __name__:
+    main()
