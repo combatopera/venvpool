94a1c4b3734adb3bcbe663cdf8a4b455aaa21b2f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-01-19 16:48:49 +0000

    support https

--- pyvenimpl/projectinfo.py
+++ pyvenimpl/projectinfo.py
@@ -78,8 +78,7 @@ class ProjectInfo:
     def descriptionandurl(self):
         import urllib.request, json, re, subprocess
         originurl = subprocess.check_output(['git', 'remote', 'get-url', 'origin'], cwd = self.projectdir).decode()
-        print(originurl)
-        urlpath = re.search('^git@github[.]com:(.+/.+)[.]git$', originurl).group(1)
+        urlpath = re.search('^(?:git@github[.]com:|https://github[.]com/)(.+/.+)[.]git$', originurl).group(1)
         with urllib.request.urlopen("https://api.github.com/repos/%s" % urlpath) as f:
             return json.loads(f.read().decode())['description'], "https://github.com/%s" % urlpath
 
