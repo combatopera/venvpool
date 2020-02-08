e87d7ced57e8a664487d8e42a8e88cd46a364c9f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-02-08 22:47:12 +0000

    obsolete

--- pyvenimpl/miniconda.py
+++ pyvenimpl/miniconda.py
@@ -19,50 +19,6 @@ from .pipify import pipify
 from .projectinfo import ProjectInfo
 import os, subprocess
 
-class PathExistsException(Exception): pass
-
-class Miniconda:
-
-    condaversion = '4.3.21'
-    opt = os.path.join(os.path.expanduser('~'), 'opt')
-
-    def __init__(self, pyversion, title, dirname, envkey):
-        self.pyversion = pyversion
-        self.scriptname = "%s-%s-Linux-x86_64.sh" % (title, self.condaversion)
-        self.target = os.path.join(self.opt, dirname)
-        self.envkey = envkey
-
-    def installifnecessary(self, deps):
-        if self.envkey in os.environ:
-            return # Already installed.
-        for path in self.scriptname, self.target:
-            if os.path.exists(path):
-                raise PathExistsException(path) # Panic.
-        subprocess.check_call(['wget', '--no-verbose', "http://repo.continuum.io/miniconda/%s" % self.scriptname])
-        command = ['bash', self.scriptname, '-b', '-p', self.target]
-        subprocess.check_call(command)
-        os.remove(self.scriptname)
-        command = [os.path.join(self.target, 'bin', 'pip'), 'install', 'pyflakes', 'nose'] # XXX: Why not pyparsing?
-        command.extend(deps)
-        subprocess.check_call(command)
-        os.environ[self.envkey] = self.target
-
-    def executable(self):
-        return os.path.join(os.environ[self.envkey], 'bin', 'python')
-
-class Jython:
-
-    pyversion = 'jython'
-
-    def executable(self):
-        return 'jython'
-
-pyversiontominiconda = {info.pyversion: info for info in [
-    Miniconda(2, 'Miniconda2', 'miniconda', 'MINICONDA_HOME'),
-    Miniconda(3, 'Miniconda3', 'miniconda3', 'MINICONDA3_HOME'),
-    Jython(),
-]}
-
 def executable(info, pyversion):
     venvpath = os.path.join(info.projectdir, '.pyven', str(pyversion))
     if not os.path.exists(venvpath):
