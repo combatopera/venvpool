826670a406702adca4ddb33596a87e8f6dac87bf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-02-17 21:23:54 +0000

    mv

--- pyvenimpl/travis_ci.py
+++ pyvenimpl/travis_ci.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pyven.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyvenimpl import projectinfo, runtests
+from . import projectinfo, runtests
 import os, subprocess
 
 class Workspace:
