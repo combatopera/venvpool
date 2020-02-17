fc9270f498c25a77825e60b06da00da5df90a310
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-02-17 21:33:08 +0000

    ren

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pyven.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyvenimpl.tests import main
+from pyven.tests import main
 import sys
 
 if '__main__' == __name__:
