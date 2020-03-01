ee12f17b814f28b49b21fa3886ceb46219a0dbae
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-01 16:50:36 +0000

    install same pyven in minivenv

--- pyven/minivenv.py
+++ pyven/minivenv.py
@@ -16,10 +16,11 @@
 # along with pyven.  If not, see <http://www.gnu.org/licenses/>.
 
 from .pipify import pipify
-from .projectinfo import ProjectInfo, ProjectInfoNotFoundException
+from .projectinfo import ProjectInfo
+from pkg_resources import get_distribution
 import os, subprocess
 
-def executable(info, pyversion):
+def bindir(info, pyversion):
     venvpath = os.path.join(info.projectdir, '.pyven', str(pyversion))
     if not os.path.exists(venvpath):
         subprocess.check_call(['virtualenv', '-p', "python%s" % pyversion, venvpath])
@@ -30,13 +31,15 @@ def executable(info, pyversion):
                 if name not in editables:
                     editables[name] = j = ProjectInfo(os.path.join(workspace, name))
                     addprojects(j)
-        try:
-            pyveninfo = ProjectInfo(__file__)
-        except ProjectInfoNotFoundException:
-            pyveninfo = dict(projects = [], deps = []) # Non-editable pyven install should already have all deps.
-        for i in info, pyveninfo:
-            addprojects(i)
+        addprojects(info)
+        reqs = info['deps'] # A new list.
+        pyvenname = 'pyven'
+        pyvendist = get_distribution(pyvenname)
+        if 'WORKING' == pyvendist.version:
+            addprojects(dict(projects = [pyvenname]))
+        else:
+            reqs.append(str(pyvendist.as_requirement()))
         for i in editables.values():
             pipify(i, False)
-        subprocess.check_call([os.path.join(venvpath, 'bin', 'pip'), 'install'] + pyveninfo['deps'] + info['deps'] + sum((['-e', i.projectdir] for i in editables.values()), []))
-    return os.path.join(venvpath, 'bin', 'python')
+        subprocess.check_call([os.path.join(venvpath, 'bin', 'pip'), 'install'] + reqs + sum((['-e', i.projectdir] for i in editables.values()), []))
+    return os.path.join(venvpath, 'bin')
