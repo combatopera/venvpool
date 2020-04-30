47da09ac40af802f625d510014265e624e242be2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-04-30 21:48:53 +0100

    require and use pyxbld files

--- pyven/pipify.py
+++ pyven/pipify.py
@@ -30,11 +30,16 @@ packages = setuptools.find_packages()
 
 def ext_modules():
     def g():
+        suffix = '.pyx'
         for package in packages:
             dirpath = package.replace('.', os.sep)
             for name in os.listdir(dirpath):
-                if name.endswith('.pyx'):
-                    yield os.path.join(dirpath, name)
+                if name.endswith(suffix):
+                    path = os.path.join(dirpath, name)
+                    g = {}
+                    with open(path + 'bld') as f:
+                        exec(f.read(), g)
+                    yield g['make_ext'](package + '.' + name[:-len(suffix)], path)
     paths = list(g())
     if paths:
         from Cython.Build import cythonize
