037da42ee523a64bff395dd2cb9638e4bd6e8d94
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-09 20:46:43 +0100

    comment

--- pyven/__init__.py
+++ pyven/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with pyven.  If not, see <http://www.gnu.org/licenses/>.
 
-workingversion = 'WORKING'
+workingversion = 'WORKING' # XXX: Just use None?
