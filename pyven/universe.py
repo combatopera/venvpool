38813a52dd9a0beb576a188d190a6f56be49ef82
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-11-28 20:32:30 +0000

    that works

--- pyven/universe.py
+++ pyven/universe.py
@@ -73,7 +73,7 @@ class Universe:
                     i = bisect(releases, release)
                     if i < len(releases):
                         yield "%s < %s" % (self.qname, lookup[releases[i]])
-            lookup = self._project(self.cname).releasetocudfversion
+            lookup = self._project(self.cname, self.req.specifier.filter).releasetocudfversion
             releases = list(lookup)
             for s in self.req.specifier:
                 release = parse_version(s.version)
@@ -135,6 +135,10 @@ class Universe:
             self.cudfversiontodepends = {}
             self.name = name
 
+        def fetch(self, filter):
+            for release in filter(self.releasetocudfversion):
+                self.dependsof(self.releasetocudfversion[release])
+
         def dependsof(self, cudfversion):
             try:
                 return self.cudfversiontodepends[cudfversion]
@@ -159,11 +163,10 @@ class Universe:
 
         def __init__(self, info):
             self.name = info.config.name
-            self.depends = [self.Depend(r) for r in info.parsedremoterequires()]
+            self.cudfversiontodepends = {1: [self.Depend(r) for r in info.parsedremoterequires()]}
 
         def dependsof(self, cudfversion):
-            assert 1 == cudfversion
-            return self.depends
+            return self.cudfversiontodepends[cudfversion]
 
         def toreq(self, cudfversion):
             assert 1 == cudfversion
@@ -171,22 +174,28 @@ class Universe:
     def __init__(self, editableinfos):
         self.projects = {p.name: p for p in map(self.EditableProject, editableinfos)}
 
-    def _project(self, name):
+    def _project(self, name, fetchfilter):
         try:
-            return self.projects[name]
+            p = self.projects[name]
         except KeyError:
             log.info("Fetch: %s", name)
             with urlopen("https://pypi.org/pypi/%s/json" % name) as f:
                 self.projects[name] = p = self.PypiProject(name, json.load(f)['releases'])
-            return p
+        p.fetch(fetchfilter)
+        return p
 
     def writecudf(self, f):
         done = set()
-        while done < self.projects.keys():
-            projects = [p for name, p in self.projects.items() if name not in done]
-            for p in projects:
-                # TODO: Elide releases that aren't in any requirement.
-                for cudfversion, release in p.cudfversiontorelease.items():
+        while True:
+            todo = {(cname, cudfversion) for cname in self.projects for cudfversion in self.projects[cname].cudfversiontodepends}
+            if done >= todo:
+                break
+            newdone = set()
+            for cname, p in list(self.projects.items()):
+                for cudfversion in p.cudfversiontodepends:
+                    if (cname, cudfversion) in done:
+                        continue
+                    release = p.cudfversiontorelease[cudfversion]
                     try:
                         dependsstr = ', '.join(d.cudfstr() for d in p.dependsof(cudfversion))
                         f.write('# %s %s\n' % (p.name, release))
@@ -197,7 +206,8 @@ class Universe:
                         f.write('\n')
                     except UnrenderableException as e:
                         log.warning("Exclude %s==%s because: %s", p.name, release, e)
-            done.update(p.name for p in projects)
+                    newdone.add((cname, cudfversion))
+            done.update(newdone)
         f.write('request: \n') # Space is needed apparently!
         f.write('install: %s\n' % ', '.join(quote(name) for name, p in self.projects.items() if p.editable))
 
