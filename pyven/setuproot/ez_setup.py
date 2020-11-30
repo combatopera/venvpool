3cf25d48ab2fad13e1a1f9301f522e0913e38a46
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-11-30 22:12:30 +0000

    why not

--- pyven/setuproot/ez_setup.py
+++ pyven/setuproot/ez_setup.py
@@ -15,5 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with pyven.  If not, see <http://www.gnu.org/licenses/>.
 
-def use_setuptools():
+def use_setuptools(*args, **kwargs):
     pass # Do nothing.
