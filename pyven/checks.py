5fbafc38f8f87a5abb41b29d559a397916a74fc8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-11-30 00:06:50 +0000

    pass in --name

--- pyven/checks.py
+++ pyven/checks.py
@@ -126,7 +126,8 @@ class EveryVersion:
             assert not status
 
 def getsetupkwargs(setuppath):
-    return eval(subprocess.check_output([sys.executable, fakesetup.__file__, os.path.basename(setuppath)], cwd = os.path.dirname(setuppath)))
+    # Use --name to invoke as little as possible, and we expect no output from it due to patched setup function:
+    return eval(subprocess.check_output([sys.executable, fakesetup.__file__, os.path.basename(setuppath), '--name'], cwd = os.path.dirname(setuppath)))
 
 def setuptoolsinfo(setuppath):
     with openresource(__name__, 'setuptools.arid') as f:
