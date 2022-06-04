32cc4ec8832b626cb92bcab5f66d908e7759d8a1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2022-06-04 18:51:35 +0100

    cool that works

---
+++ pyven/test_minivenv.py
@@ -0,0 +1,29 @@
+# Copyright 2013, 2014, 2015, 2016, 2017, 2020 Andrzej Cichocki
+
+# This file is part of pyven.
+#
+# pyven is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyven is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyven.  If not, see <http://www.gnu.org/licenses/>.
+
+from .minivenv import oserrors, _osop
+from unittest import TestCase
+import errno, os
+
+class TestMiniVenv(TestCase):
+
+    def test_oserrors(self):
+        try:
+            _osop(os.rmdir, 'definitely-does-not-exist-no-way')
+        except oserrors[errno.ENOENT] as e:
+            x = e
+        self.assertEqual(errno.ENOENT, x.errno)
