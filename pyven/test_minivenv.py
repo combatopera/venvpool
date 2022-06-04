69cdc9ac0799292ec4f4712f11d8c965868fae6a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2022-06-04 19:00:37 +0100

    reliabler

--- pyven/test_minivenv.py
+++ pyven/test_minivenv.py
@@ -15,15 +15,17 @@
 # You should have received a copy of the GNU General Public License
 # along with pyven.  If not, see <http://www.gnu.org/licenses/>.
 
-from .minivenv import oserrors, _osop
+from .minivenv import oserrors, _osop, TemporaryDirectory
 from unittest import TestCase
 import errno, os
 
 class TestMiniVenv(TestCase):
 
     def test_oserrors(self):
-        try:
-            _osop(os.rmdir, 'definitely-does-not-exist-no-way')
-        except oserrors[errno.ENOENT] as e:
-            x = e
-        self.assertEqual(errno.ENOENT, x.errno)
+        with TemporaryDirectory() as tempdir:
+            try:
+                _osop(os.rmdir, os.path.join(tempdir, 'does-not-exist'))
+            except oserrors[errno.ENOENT] as e:
+                self.assertEqual(errno.ENOENT, e.errno)
+            else:
+                self.fail()
